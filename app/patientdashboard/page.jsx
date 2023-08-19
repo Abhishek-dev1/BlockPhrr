e26366d277d7038e7f7f5431c49929eadca616f2
Abhishek <88185067+Abhishek-dev1@users.noreply.github.com>
2023-08-19 10:05:48 +0530

    Update-Details

--- app/patientdashboard/page.jsx
+++ app/patientdashboard/page.jsx
@@ -72,9 +72,12 @@ const UserDashboard = () => {
       <p className="mb-2">Aadhar Card Number: {userDetails.aadharNumber}</p>
     </div>
     <div className="flex justify-center mt-6">
-      <button className="px-4 py-2 w-52 bg-blue-500 text-white rounded hover:bg-blue-600 mr-4">
-        Update Details
-      </button>
+      <Link
+            href="/patientdashboard/updateDetails"
+            className="px-4 text-center py-2 w-52 bg-green-500 text-white rounded hover:bg-green-600 mr-4"
+          >
+            Update Details
+      </Link>
       <button className="px-4 py-2 w-52 bg-blue-500 text-white rounded hover:bg-blue-600">
         View Details
       </button>
